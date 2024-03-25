a2195ca2353c7dd04d315513ae2d04e1eed1694a
najiibmohamed11 <xaawomaxamuudkaahiye@gmail.com>
2024-03-25 17:40:04 +0300

    welcome screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:sweet_app/pages/cart/cart%20model/cartmodel.dart';
 import 'package:sweet_app/pages/cart/cart.dart';
 import 'package:sweet_app/pages/favorate/favorate.dart';
 import 'package:sweet_app/pages/upload/uploadscreen.dart';
+import 'package:sweet_app/pages/welcomescreen/welcome.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'firebase_options.dart';
 
@@ -26,12 +27,13 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [ChangeNotifierProvider(create: (context) => cartopration())],
       child: MaterialApp(
-        initialRoute: Home.id,
+        initialRoute: WelcomeScreen.id,
         routes: {
           Uploadscreen.id: (context) => Uploadscreen(),
           Home.id: (context) => Home(),
           Cart.id: (context) => Cart(),
-          Favorate.id: (context) => Favorate()
+          Favorate.id: (context) => Favorate(),
+          WelcomeScreen.id: (context) => WelcomeScreen()
         },
       ),
     );
---
+++ lib/pages/welcomescreen/welcome.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+  static String id = "welcome";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFF9CBDF), // #F9CBDF
+                Color.fromARGB(255, 216, 217, 225), // #EBE4F5
+              ],
+              stops: [0.1, 0.4],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                width: 350,
+                height: 350,
+                child: Image.asset("images/firstapp.png"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'welcome to flutter sweets',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSerif(
+                      fontSize: 35.0, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Satisfy your cravings with our exquisite selection of treats, meticulously crafted for your enjoyment.',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    letterSpacing: 1.5,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 45.0,
+              ),
+              athenticationbuttons(
+                name: "  Logign",
+                color: Colors.black,
+                onTap: () {},
+                textcolor: Colors.white,
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              athenticationbuttons(
+                name: "  Register",
+                color: Color(0xffAFAFAF),
+                onTap: () {},
+                textcolor: Colors.black,
+              )
+            ],
+          )),
+    ));
+  }
+}
+
+class athenticationbuttons extends StatelessWidget {
+  athenticationbuttons(
+      {super.key, this.name, this.color, this.onTap, this.textcolor});
+  String? name;
+  Color? color;
+  Color? textcolor;
+  void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 25.0),
+        width: double.infinity,
+        height: 80.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12), color: color),
+        child: Center(
+          child: Text(
+            name!,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w500,
+              color: textcolor, // Change the color here
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
